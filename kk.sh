ac4b25a8e81bd015d7710c730efb02e250eef4f3
luisgarciacheckmarx <98318336+luisgarciacheckmarx@users.noreply.github.com>
2023-02-09 13:24:13 +0100

    Update kk.sh

--- kk.sh
+++ kk.sh
@@ -1,2 +1,16 @@
+
+#!/bin/bash
+
+VAR1=$1
+VAR2="misupersecret"
+
+if [ "$VAR1" = "$VAR2" ]; then
+    echo "Strings are equal."
+else
+    echo "Strings are not equal."
+    echo " VAR1 $VAR1 "
+fi
+
+
 echo "hola desde sh" > ./kaka.txt
 echo $* >> kaka.txt
